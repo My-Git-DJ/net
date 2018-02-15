c168c088220b1ea6e94e169d74837f756721060a
king <924935279@qq.com>
2018-02-15 23:01:44 +0800

    # 异步数据库模块

--- moba_server/apps/test/main.cc
+++ moba_server/apps/test/main.cc
@@ -13,8 +13,40 @@ using namespace std;
 #include "../../utils/logger.h"
 #include "../../utils/time_list.h"
 
+#include "../../database/mysql_wrapper.h"
+
 static void on_logger_timer(void* update) {
 	log_debug("on_logger_timer");
+}
+
+static void 
+on_query_cb(const char* err, std::vector<std::vector<std::string>> *result) {
+	if (err != NULL) {
+		printf("%s\n", err);
+		return;
+	}
+
+	printf("success!!!\n");
+}
+
+static void
+on_open_cb(const char* err, void* context) {
+	if (err != NULL) {
+		printf("%s\n", err);
+		return;
+	}
+
+	printf("connect success\n");
+
+	//mysql_wrapper::query(context, "update user set user_name = \"blake\" where user_id = 10", on_query_cb);
+	mysql_wrapper::query(context, "select * from user", on_query_cb);
+
+	//mysql_wrapper::close(context);
+}
+
+static void 
+test_db() {
+	mysql_wrapper::connect("127.0.0.1", 3306, "new_test", "root", "king199241", on_open_cb);
 }
 
 int main(int argc, char** argv) {
@@ -23,6 +55,9 @@ int main(int argc, char** argv) {
 
 	//logger::init("logger/gateway/", "gateawy", true);
 	//schedule(on_logger_timer, NULL, 3000, -1);
+
+	test_db();
+
 
 
 	netbus::instance()->init();

fe507764f2e259128bd0b02521ae7e78304c5373
DengJin <924935279@qq.com>
2018-02-23 16:10:03 +0800

    # 异步redis模块

--- moba_server/apps/test/main.cc
+++ moba_server/apps/test/main.cc
@@ -14,6 +14,7 @@ using namespace std;
 #include "../../utils/time_list.h"
 
 #include "../../database/mysql_wrapper.h"
+#include "../../database/redis_wrapper.h"
 
 static void on_logger_timer(void* update) {
 	log_debug("on_logger_timer");
@@ -49,6 +50,35 @@ test_db() {
 	mysql_wrapper::connect("127.0.0.1", 3306, "new_test", "root", "king199241", on_open_cb);
 }
 
+static void
+on_redis_query_cb(const char* err,redisReply* result) {
+	if (err) {
+		printf("%s\n", err);
+		return;
+	}
+	printf("success\n");
+}
+
+static void
+on_redis_open_cb(const char* err, void* context) {
+	if (err != NULL) {
+		printf("%s\n", err);
+		return;
+	}
+
+	printf("connect success\n");
+
+	//mysql_wrapper::query(context, "update user set user_name = \"blake\" where user_id = 10", on_query_cb);
+	redis_wrapper::query(context, "select 1", on_redis_query_cb);
+
+	//redis_wrapper::close_redis(context);
+}
+
+static void 
+test_redis() {
+	redis_wrapper::connect("127.0.0.1", 6379, on_redis_open_cb);
+}
+
 int main(int argc, char** argv) {
 	proto_man::init(PROTO_BUF);
 	init_pf_cmd_map();
@@ -56,8 +86,8 @@ int main(int argc, char** argv) {
 	//logger::init("logger/gateway/", "gateawy", true);
 	//schedule(on_logger_timer, NULL, 3000, -1);
 
-	test_db();
-
+	//test_db();
+	test_redis();
 
 
 	netbus::instance()->init();
---
+++ moba_server/database/redis_wrapper.h
@@ -0,0 +1,10 @@
+#pragma once
+
+struct redisReply;
+
+class redis_wrapper {
+public:
+	static void connect(char* ip, int port, void(*opne_cb)(const char* err, void* context));
+	static void close_redis(void* context);
+	static void query(void* context, char* cmd, void(*query_cb)(const char* err, redisReply* result));
+};

e4c90b122dce9b4655fe87313b8f83c7fde374ca
DengJin <924935279@qq.com>
2018-02-27 10:53:07 +0800

    # 日志 和time相关调整

--- moba_server/utils/time_list.h
+++ moba_server/utils/time_list.h
@@ -8,15 +8,17 @@ extern "C" {
 	// on_timer是一个回掉函数,当timer触发的时候调用;
 	// udata: 是用户传的自定义的数据结构;
 	// on_timer执行的时候 udata,就是你这个udata;
-	// after_sec: 多少秒开始执行;
+	// after_sec: 多少秒开始第一次执行;
 	// repeat_count: 执行多少次, repeat_count == -1一直执行;
+	//repeat_msec : 后面每隔多长时间执行一次
 	// 返回timer的句柄;
 	struct timer;
 	struct timer*
-	schedule(void(*on_timer)(void* udata),
+	schedule_repeat(void(*on_timer)(void* udata),
 	         void* udata,
 			 int after_msec,
-			 int repeat_count);
+			 int repeat_count,
+			 int repeat_msec);
 
 
 	// 取消掉这个timer;
